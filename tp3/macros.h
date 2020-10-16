754738b831f449c30089fc3402445a98502b22bd
Clara Moreira <up201806309@fe.up.pt>
2020-10-16 15:12:44 +0100

    added tp3

---
+++ tp3/macros.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+#define FLAG 0x7e
+
+//A
+#define A_CMD_EMISSOR 0x03
+#define A_RSP_RECETOR 0x03
+#define A_RSP_EMISSOR 0x01
+#define A_CMD_RECETOR 0x01
+
+//C
+#define SET 0x03
+#define DISC 0x0b
+#define UA 0x07
+
---
+++ tp3/statemachine.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include "macros.h"
+
+typedef enum State{
+
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    STOP
+
+} State;
+
+
+typedef struct StateMachine{
+
+    State state;
+    char byte;
+    char flag;
+    char a;
+    char c;
+
+
+}StateMachine;
+
+
+
+void changeState(StateMachine machine);
